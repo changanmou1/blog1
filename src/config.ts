9e2fe761d3df590445c93ddca7652455f20d979b
changanmou1 <104680290+changanmou1@users.noreply.github.com>
2025-09-20 11:16:48 +0000

    Change web

--- src/config.ts
+++ src/config.ts
@@ -288,7 +288,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://chengdieyi.dpdns.org",
+		envId: "https://chengdieyi.dpdns.org/demo.html",
 		lang: "zh_CN", // 设置 Twikoo 评论系统语言为英文
 	},
 };
